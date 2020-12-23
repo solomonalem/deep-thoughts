0e0cdd00d4d7ccd6bb7d4298075e1ee44c853d1e
Solomon <solomonmeresa8@gmail.com>
2020-12-22 22:59:11 -0800

    adds concurrency for using single terminal for both servers

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -20,6 +21,16 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 
 db.once('open', () => {
   app.listen(PORT, () => {
