7caa6ed3f0db0787483c6039b152314ded044061
Solomon <solomonmeresa8@gmail.com>
2020-12-23 02:07:17 -0800

    adds react-router

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,8 +1,50 @@
 import gql from 'graphql-tag';
 
+// USER
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+      friendCount
+      friends {
+        _id
+        username
+      }
+      thoughts {
+        _id
+        thoughtText
+        createdAt
+        reactionCount
+      }
+    }
+  }
+`;
+
+// THOUGHTS
 export const QUERY_THOUGHTS = gql`
   query thoughts($username: String) {
     thoughts(username: $username) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+        createdAt
+        username
+        reactionBody
+      }
+    }
+  }
+`;
+
+// SINGLE THOUGHT
+export const QUERY_THOUGHT = gql`
+  query thought($id: ID!) {
+    thought(_id: $id) {
       _id
       thoughtText
       createdAt
