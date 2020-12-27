4ee22e0ddf83bc182cd2306759e417e5ca4a4728
Solomon <solomonmeresa8@gmail.com>
2020-12-26 20:11:19 -0800

    adds reaction-form

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -40,3 +40,36 @@ export const ADD_FRIEND = gql`
     }
   }
 `;
+
+// Add Thoughts
+
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+// Add Reaction
+export const ADD_REACTION = gql`
+  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
